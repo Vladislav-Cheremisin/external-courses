6c9c73110ca53287d9fe30ebb0294a20274c0098
Vladislav Cheremisin <vladislav.cheremisin@artimpex.kz>
2021-09-21 18:55:07 +0300

    feat: complete task-03

---
+++ src/ex2_js-basics-part2/task-03.js
@@ -0,0 +1,19 @@
+const oddAndEvenCounter = (array) => {
+  let evenCounter = 0;
+  let oddCounter = 0;
+  let nullCounter = 0;
+  array.forEach((element) => {
+    if (typeof element === 'number' && !Number.isNaN(element)) {
+      if (element % 2 === 0 && element !== 0) {
+        evenCounter += 1;
+      } else if (element % 2 !== 0 && element !== 0) {
+        oddCounter += 1;
+      } else {
+        nullCounter += 1;
+      }
+    }
+  });
+  return [evenCounter, oddCounter, nullCounter];
+};
+
+module.exports = oddAndEvenCounter;
