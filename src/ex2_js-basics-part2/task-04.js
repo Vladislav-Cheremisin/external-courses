936a4f0ba031d6332ab518053f2d40c4b6ef38fb
Vladislav Cheremisin <vladislav.cheremisin@artimpex.kz>
2021-09-23 12:06:10 +0300

    feat: add const with first array element in task-04. Add correct linebrakes

--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -1,9 +1,12 @@
 const isArrElementsTheSame = (array) => {
+  const ARRAY_FIRST_ELEMENT = array[0];
+
   for (let i = 0; i < array.length; i += 1) {
-    if (array[i] !== array[0]) {
+    if (array[i] !== ARRAY_FIRST_ELEMENT) {
       return false;
     }
   }
+
   return true;
 };
 
