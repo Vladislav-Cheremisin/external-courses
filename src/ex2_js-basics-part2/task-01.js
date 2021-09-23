40543d2a27cd46975748a8cdcbf1bcd462c762d0
Vladislav Cheremisin <vladislav.cheremisin@artimpex.kz>
2021-09-23 11:37:55 +0300

    refactor: add correct linebrakes in task-01

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -2,9 +2,11 @@ const isStringOrNumber = (data) => {
   if (typeof data === 'string') {
     return 'string';
   }
+
   if (typeof data === 'number' && !Number.isNaN(data)) {
     return 'number';
   }
+
   return undefined;
 };
 
