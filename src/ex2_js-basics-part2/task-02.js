c2aeb633a27cb913d49946ede91e3f4517944408
Vladislav Cheremisin <vladislav.cheremisin@artimpex.kz>
2021-09-23 11:44:51 +0300

    refactor: add correct linebrakes in task-02

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,9 +1,11 @@
 const arrayElementsCount = (array) => {
   let elementsCount = 0;
+
   for (let i = 0; i < array.length; i += 1) {
     console.log(array[i]);
     elementsCount += 1;
   }
+
   console.log(elementsCount);
 };
 
