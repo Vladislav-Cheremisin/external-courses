679e197feee780e8e731fd88720cfa5168901e1c
Vladislav Cheremisin <vladislav.cheremisin@artimpex.kz>
2021-09-23 11:48:58 +0300

    feat: make function in task-02 easier

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,12 +1,9 @@
 const arrayElementsCount = (array) => {
-  let elementsCount = 0;
-
   for (let i = 0; i < array.length; i += 1) {
     console.log(array[i]);
-    elementsCount += 1;
   }
 
-  console.log(elementsCount);
+  console.log(array.length);
 };
 
 module.exports = arrayElementsCount;
