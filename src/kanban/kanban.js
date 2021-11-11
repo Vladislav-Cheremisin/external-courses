85ccae8974093a106029b832a7fbc2a242cdc145
Vladislav <vladislav.cheremisin@artimpex.kz>
2021-11-12 00:52:56 +0300

    feat: add kanban-lists, add possibility to create new list-items

--- src/kanban/kanban.js
+++ src/kanban/kanban.js
@@ -32,3 +32,125 @@ const hideUserPopUp = (event) => {
 
 userProfile.addEventListener('click', showUserPopUp);
 document.body.addEventListener('mouseup', hideUserPopUp);
+
+/* Task 13 */
+
+const mainWrapper = document.querySelector('.main');
+let boardMocks = null;
+
+if (localStorage.getItem('boardMocks') !== null) {
+  boardMocks = JSON.parse(localStorage.getItem('boardMocks'));
+} else {
+  boardMocks = [
+    {
+      title: 'Backlog',
+      issues: [
+        {
+          id: 'task1',
+          name: 'backlog task example',
+        }
+      ],
+    },
+    {
+      title: 'Ready',
+      issues: [
+        {
+          id: 'task1',
+          name: 'ready task example',
+        }
+      ],
+    },
+    {
+      title: 'In Progress',
+      issues: [
+        {
+          id: 'task1',
+          name: 'in progress task example',
+        }
+      ],
+    },
+    {
+      title: 'Finished',
+      issues: [
+        {
+          id: 'task1',
+          name: 'finished task example',
+        }
+      ],
+    },
+  ];
+}
+
+function paintLists() {
+  mainWrapper.innerHTML = '';
+  localStorage.setItem('boardMocks', JSON.stringify(boardMocks));
+
+  boardMocks.forEach(element => {
+    const list = document.createElement('section');
+    const listHeader = document.createElement('h1');
+    const listPopUpBtn = document.createElement('button');
+    const listTaskWrapper = document.createElement('ul');
+    const listAddCardBtn = document.createElement('button');
+    
+    list.className = 'kanban-list';
+    listHeader.className = 'kanban-list__header';
+    listHeader.innerText = element.title;
+    listPopUpBtn.className = 'kanban-list__pop-up-btn';
+    listPopUpBtn.innerText = '•••';
+    listTaskWrapper.className = 'kanban-list__task-wrapper';
+    listAddCardBtn.className = 'kanban-list__add-card-btn';
+    listAddCardBtn.innerText = 'Add Card';
+
+    list.append(listHeader);
+    list.append(listPopUpBtn);
+
+    element.issues.forEach(issue => {
+      const newIssue = document.createElement('li');
+
+      newIssue.className = 'kanban-list__task';
+      newIssue.id = issue.id;
+      newIssue.innerText = issue.name;
+
+      listTaskWrapper.append(newIssue);
+    })
+
+    list.append(listTaskWrapper);
+    list.append(listAddCardBtn);
+    mainWrapper.append(list);
+  })
+}
+
+paintLists(boardMocks);
+
+let addCardBtns = document.querySelectorAll('.kanban-list__add-card-btn');
+let listsTaskWrappers = document.querySelectorAll('.kanban-list__task-wrapper');
+
+function createNewListItem() {
+  const newListInput = document.createElement('input');
+
+  newListInput.className = 'kanban-list__task-input';
+  newListInput.setAttribute('type', 'text');
+  
+  listsTaskWrappers[0].append(newListInput);
+  newListInput.focus();
+
+  newListInput.addEventListener('blur', () => {
+    if (newListInput.value === '') {
+      listsTaskWrappers[0].removeChild(newListInput);
+    } else {
+      const newListItem = {
+        id: `task${boardMocks[0].issues.length + 1}`,
+        name: newListInput.value,
+      }
+
+      boardMocks[0].issues.push(newListItem);
+      listsTaskWrappers[0].removeChild(newListInput);
+      paintLists(boardMocks);
+      addCardBtns = document.querySelectorAll('.kanban-list__add-card-btn');
+      listsTaskWrappers = document.querySelectorAll('.kanban-list__task-wrapper');
+      addCardBtns[0].addEventListener('click', createNewListItem);
+    }
+  })
+}
+
+addCardBtns[0].addEventListener('click', createNewListItem);
