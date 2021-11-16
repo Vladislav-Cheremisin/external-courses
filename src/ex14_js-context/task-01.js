30cfdf69f4d74d69d0949bcce21ca2cd93a0aa82
Vladislav <vladislav.cheremisin@artimpex.kz>
2021-11-16 14:07:41 +0300

    init: start ex14_js-context. Complete task-01

---
+++ src/ex14_js-context/task-01.js
@@ -0,0 +1,56 @@
+function Calculator() {
+  this.result = 0;
+  this.add = (num) => {
+    if (num) {
+      this.result += num;
+    }
+
+    return this;
+  };
+  this.subtract = (num) => {
+    if (num) {
+      this.result -= num;
+    }
+
+    return this;
+  };
+  this.divide = (num) => {
+    if (num) {
+      this.result /= num;
+    }
+
+    return this;
+  };
+  this.multiply = (num) => {
+    if (num) {
+      this.result *= num;
+    }
+
+    return this;
+  };
+  this.reset = () => {
+    this.result = 0;
+
+    return this;
+  };
+  this.setState = (num) => {
+    if (num) {
+      this.result = num;
+    }
+
+    return this;
+  };
+  this.fetchData = (callback) => {
+    if (callback) {
+      this.result = 500;
+      callback(this.result);
+    }
+
+    return this;
+  };
+  this.getResult = () => this.result;
+}
+
+const calculator = new Calculator();
+
+module.exports = calculator;
